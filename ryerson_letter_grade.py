531b750c092559b489b420f5a7efe409bd8633c8
s3kay <48930492+s3kay@users.noreply.github.com>
2019-03-25 22:29:09 +0000

    Add files via upload

---
+++ ryerson_letter_grade.py
@@ -0,0 +1,28 @@
+def ryerson_letter_grade(pct):
+
+    if pct >= 90:
+        print('A+')
+    elif pct >= 85:
+        print ('A')
+    elif pct >= 80:
+        print ('A-')
+    elif pct >= 77:
+        print ('B+')
+    elif pct >= 73:
+        print ('B')
+    elif pct >= 70:
+        print ('B-')
+    elif pct >= 67:
+        print ('C+')
+    elif pct >= 63:
+        print ('C')
+    elif pct >= 60:
+        print ('C-')
+    elif pct >= 57:
+        print ('D+')
+    elif pct >= 53:
+        print ('D')
+    elif pct >= 50:
+        print ('D-')
+    elif pct >= 0:
+        print ('F')
