f6db2fe4d072f03eb3cceb584f9b861cbbf9ecef
s3kay <48930492+s3kay@users.noreply.github.com>
2019-03-25 22:41:59 +0000

    Update ryerson_letter_grade.py

--- ryerson_letter_grade.py
+++ ryerson_letter_grade.py
@@ -1,28 +1,28 @@
 def ryerson_letter_grade(pct):
 
     if pct >= 90:
-        print('A+')
+        return('A+')
     elif pct >= 85:
-        print ('A')
+        return ('A')
     elif pct >= 80:
-        print ('A-')
+        return ('A-')
     elif pct >= 77:
-        print ('B+')
+        return ('B+')
     elif pct >= 73:
-        print ('B')
+        return ('B')
     elif pct >= 70:
-        print ('B-')
+        return ('B-')
     elif pct >= 67:
-        print ('C+')
+        return ('C+')
     elif pct >= 63:
-        print ('C')
+        return ('C')
     elif pct >= 60:
-        print ('C-')
+        return ('C-')
     elif pct >= 57:
-        print ('D+')
+        return ('D+')
     elif pct >= 53:
-        print ('D')
+        return ('D')
     elif pct >= 50:
-        print ('D-')
+        return ('D-')
     elif pct >= 0:
-        print ('F')
+        return ('F')
